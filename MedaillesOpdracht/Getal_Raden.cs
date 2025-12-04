a961ec40b014ef20bc72385b9fffb1dde53c0f1f
Y.Ouallal <2235702@talnet.nl>
2025-12-04 11:56:17 +0100

    Update Getal_Raden.cs

--- MedaillesOpdracht/Getal_Raden.cs
+++ MedaillesOpdracht/Getal_Raden.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,15 @@ namespace MedaillesOpdracht
     {
         public void Start()
         {
+            int[] numberArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            int number = numberArray[8];
             bool running = false;
-            int maxnumber = 0;
-            int attempts = 3;
+            int maxnumber = 20;
+            int attempts = 5;
 
             while (running)
             {
-                if (maxnumber == 0)
+                if (maxnumber <= 20)
                 {
                     Console.WriteLine("\nRaad het getal.");
                     maxnumber = maxnumber + 1;
@@ -29,9 +32,24 @@ namespace MedaillesOpdracht
                     Console.WriteLine($"\nOngeldig invoer. Dit kost één poging. Pogingen over: {attempts}");
                 }
                 Console.Clear();
+                if (answer == 8)
+                {
+                    running = false;
+                    Console.WriteLine("\nGefeliciteerd! Je hebt het getal geraden!");
+                }
+                else if (answer > 8)
+                {
+                    Console.Clear();
+                    attempts = attempts - 1;
+                    Console.WriteLine("\nJe hebt het getal nog niet geraden, het is lager.");
+                }
+                else if (answer < 8)
+                {
+                    Console.Clear();
+                    attempts = attempts - 1;
+                    Console.WriteLine("\nJe hebt het getal nog niet geraden, het is hoger");
+                }
             }
-
-         
         }
     }
 }
